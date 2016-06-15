4e09a0a3164a359714348f8f8dcca503a75338b9
Antono Vasiljev <antono.vasiljev@gmail.com>
2016-06-15 11:24:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# atom-linter-escomplex package
+
+Atom linter plugin for complexity analysis of JavaScript abstract syntax trees.
+
+Based on [escomplex](https://github.com/escomplex/escomplex).
+
+
---
+++ spec/atom-linter-escomplex-spec.js
@@ -0,0 +1,17 @@
+'use babel';
+
+import AtomLinterEscomplex from '../lib/atom-linter-escomplex';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('AtomLinterEscomplex', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('atom-linter-escomplex');
+  });
+});
